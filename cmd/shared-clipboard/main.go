db45a383895ed8c58176067896fb1e14588a9c2b
Aniki <anikipelau@gmail.com>
2024-02-17 10:23:31 +0300

    add cli flags

--- cmd/shared-clipboard/main.go
+++ cmd/shared-clipboard/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	sharedclipboard "shared-clipboard"
+	"flag"
 
 	"github.com/trueaniki/gopeers"
+	"github.com/trueaniki/shared-clipboard"
 	"golang.design/x/clipboard"
 )
 
+var (
+	network  string
+	confPath string
+)
+
 func main() {
+	flag.StringVar(&network, "network", "", "network to scan in CIDR format")
+	flag.StringVar(&network, "net", "", "network to scan in CIDR format")
+
+	flag.StringVar(&confPath, "conf", "", "path to config file")
+	flag.StringVar(&confPath, "c", "", "path to config file")
+
+	flag.Parse()
+	if network == "" {
+		panic("network flag is required")
+	}
+
 	err := clipboard.Init()
 	if err != nil {
 		panic(err)
 	}
-	locals := gopeers.PingSweep("192.168.100.0/24")
+	locals := gopeers.PingSweep(network)
 	peer := gopeers.NewPeer(locals)
 	peer.Start()
 
